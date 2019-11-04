5b535e42389b25262c5f9554f8fef94410531461
ksetrin <ksetrinn@gmail.com>
2019-11-05 01:47:32 +0300

    added Feed screen

---
+++ src/screens/Feed/FeedScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+class FeedScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>feed</Text>
+      </View>
+    );
+  }
+}
+
+export default FeedScreen;
