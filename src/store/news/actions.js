d5ddfdad0aff2b0ec811624a893ba22cb0d7a7a1
ksetrin <ksetrinn@gmail.com>
2019-11-05 19:55:50 +0300

    replace fast-xml-parser to react-native-xml2js

--- src/store/news/actions.js
+++ src/store/news/actions.js
@@ -1,22 +1,21 @@
-import parser from 'fast-xml-parser';
-
 export const NEWS_FETCH_REQUEST_PENDING = 'news/fetch/request/pending';
 export const NEWS_FETCH_REQUEST_SUCCESS = 'news/fetch/request/success';
 export const NEWS_FETCH_REQUEST_FAILURE = 'news/fetch/request/failure';
 
-export const newsFetchRequest = () => async dispatch => {
+export const newsFetchRequest = () => async dispatch => { // rssFetchRequest
   console.log('newsFetchRequest');
   dispatch({type: NEWS_FETCH_REQUEST_PENDING});
   try {
     const response = await fetch('https://news.tut.by/rss/all.rss');
     const text = await response.text();
-    const {
-      rss: {channel},
-    } = parser.parse(text);
 
-    console.log('+kse-channel', channel);
+    const parseString = require('react-native-xml2js').parseString;
+    let data = null;
+    parseString(text, (err, {rss: {channel}}) => (data = channel[0])); // todo handle error
 
-    dispatch({type: NEWS_FETCH_REQUEST_SUCCESS, list: channel.item});
+    console.log('+kse-channel', data);
+
+    dispatch({type: NEWS_FETCH_REQUEST_SUCCESS, list: data.item});
   } catch (error) {
     console.log('error', error.message);
     dispatch({type: NEWS_FETCH_REQUEST_FAILURE, errorMessage: error.message});
